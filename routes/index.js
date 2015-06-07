e3323d98ad49c41f6f8306f97c1ee5802da7d1b4
Juan Belmonte Rodríguez <juanbelmonterodriguez@iMac-de-Juan-Belmonte-Rodriguez.local>
2015-06-07 21:44:28 +0200

    P2P parte 2: author

--- routes/index.js
+++ routes/index.js
@@ -12,5 +12,10 @@ router.get('/', function(req, res, next) {
 router.get('/quizes/question', quizController.question);
 router.get('/quizes/answer', quizController.answer);
 
+/* GET home page. */
+router.get('/author', function(req, res, next) {
+  res.render('author');
+});
+
 // Export
 module.exports = router;
