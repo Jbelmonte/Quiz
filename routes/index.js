0fbf9bcc0443ec229130235e79cc84c71afa80eb
Juan Belmonte Rodríguez <juanbelmonterodriguez@iMac-de-Juan-Belmonte-Rodriguez.local>
2015-06-07 20:09:53 +0200

    Primera pregunta

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express'),
+	router = express.Router(),
+	quizController = require('../controllers/quiz_controller');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Quiz' });
 });
 
+
+// Quiz
+router.get('/quizes/question', quizController.question);
+router.get('/quizes/answer', quizController.answer);
+
+// Export
 module.exports = router;
