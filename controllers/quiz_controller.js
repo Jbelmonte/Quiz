ec08027bfe0d1272b9ab6b3ea227f80012597b7a
Juan Belmonte Rodríguez <juanbelmonterodriguez@iMac-de-Juan-Belmonte-Rodriguez.local>
2015-06-11 01:14:28 +0200

    Lista de preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,19 +1,26 @@
 var models = require('../models/models.js');
 
-// GET /quizes/question
-exports.question = function (req, res) {
-	models.Quiz.findAll().then(function(quiz) {
-		res.render('quizes/question', { pregunta: quiz[0].pregunta });
+// GET /quizes?search=
+exports.index = function (req, res) {
+	models.Quiz.findAll().then(function(quizes) {
+		res.render('quizes/index', { quizes: quizes });
 	});
 };
 
-// GET /quizes/answer
+// GET /quizes/:quizId(\\d+)
+exports.show = function (req, res) {
+	models.Quiz.findById(req.params.quizId).then(function(quiz) {
+		res.render('quizes/show', { quiz: quiz });
+	});
+};
+
+// GET /quizes/:quizId(\\d+)/answer
 exports.answer = function (req, res) {
-	models.Quiz.findAll().then(function(quiz) {
+	models.Quiz.findById(req.params.quizId).then(function(quiz) {
 		var resultado = 'Incorrecta';
-		if (req.query.respuesta === quiz[0].respuesta) {
+		if (req.query.respuesta === quiz.respuesta) {
 			resultado = 'Correcta';
 		}
-		res.render('quizes/answer', { respuesta: resultado });
+		res.render('quizes/answer', { respuesta: resultado, quiz: quiz });
 	});
 };
--- routes/index.js
+++ routes/index.js
@@ -9,8 +9,9 @@ router.get('/', function(req, res, next) {
 
 
 // Quiz
-router.get('/quizes/question', quizController.question);
-router.get('/quizes/answer', quizController.answer);
+router.get('/quizes', quizController.index);
+router.get('/quizes/:quizId(\\d+)', quizController.show);
+router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
 
 /* GET home page. */
 router.get('/author', function(req, res, next) {
