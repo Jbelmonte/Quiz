a46d1b3f467d6bfdaf46e2c96250ab6177664e0c
Juan Belmonte Rodríguez <juanbelmonterodriguez@iMac-de-Juan-Belmonte-Rodriguez.local>
2015-06-11 21:13:40 +0200

    Despliegue en Heroku con Postgres

--- models/models.js
+++ models/models.js
@@ -1,9 +1,26 @@
 var path = require('path'),
 	Sequelize = require('sequelize');
 
+var url = process.env.DATABASE_URL.match(/(.*)\:\/\/(.*)?\:(.*)@(.*)\:(.*)\/(.*)/);
+var DB_name  = (url[6] || null),
+	user     = (url[2] || null),
+	pwd      = (url[3] || null),
+	protocol = (url[1] || null),
+	dialect  = (url[1] || null),
+	port     = (url[5] || null),
+	host     = (url[4] || null),
+	storage  = process.env.DATABASE_STORAGE;
+
 // BBDD
-var sequelize = new Sequelize(null, null, null,
-								{ dialect: 'sqlite', storage: 'quiz.sqlite' });
+var sequelize = new Sequelize(DB_name, user, pwd,
+							{
+								dialect: dialect,
+								protocol: protocol,
+								port: port,
+								host: host,
+							    storage: storage,
+								omitNull: true
+							});
 
 // Definición de la tabla Quiz en quiz.js
 var Quiz = sequelize.import(path.join(__dirname, 'quiz'));
